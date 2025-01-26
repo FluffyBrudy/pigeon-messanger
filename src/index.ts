7e54fb72a8fbf8745ccca89da2d4569276e5aac5
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-01-26 14:36:16 +0545

    Remove socket to separate websocket server and api server

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
 import { app } from "./app";
-import { createServer as createHttpServer } from "http";
-import { Server as newIOServer } from "socket.io";
 
-const httpServer = createHttpServer(app);
-const io = new newIOServer(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("Client connected");
-
-  socket.on("hello", (res) => {
-    console.log(res);
-  });
-});
-
-httpServer.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Listening at: http://localhost:3000");
 });
