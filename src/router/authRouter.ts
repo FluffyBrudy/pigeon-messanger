f7780f7cde704e2633471f2e7e6edb23e4a5fe6e
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-01-31 14:14:56 +0545

    add implicit login route

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { RegisterController } from "../controller/auth/RegisterController";
 import { loginValidator, registerValidator } from "../validator/auth/auth";
 import { LoginController } from "../controller/auth/LoginController";
+import { verifyAuth } from "../middleware/authVerification";
 
 const authRouter = Router();
 
 authRouter.post("/register", registerValidator, RegisterController);
 authRouter.post("/login", loginValidator, LoginController);
+authRouter.post("/silent-login", verifyAuth(), (_, res) => {
+  res.json({ data: { isAuthorized: true } });
+});
 
 export { authRouter };
