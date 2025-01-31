89fd4401977726a0601190bb21a87c2983bbc666
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-01-31 22:31:28 +0545

    Refactor authentication error handling and silent login response
    - Updated error handling in LoginController to return 401 status code for invalid credentials instead of 400.
    - Enhanced silent-login route in authRouter to return the user ID in the response, improving the client-side handling of user authentication state.
    - Added type assertion for user in the silent-login route to ensure proper type handling.

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -3,13 +3,15 @@ import { RegisterController } from "../controller/auth/RegisterController";
 import { loginValidator, registerValidator } from "../validator/auth/auth";
 import { LoginController } from "../controller/auth/LoginController";
 import { verifyAuth } from "../middleware/authVerification";
+import { ExpressUser } from "../types/common";
 
 const authRouter = Router();
 
 authRouter.post("/register", registerValidator, RegisterController);
 authRouter.post("/login", loginValidator, LoginController);
-authRouter.post("/silent-login", verifyAuth(), (_, res) => {
-  res.json({ data: { isAuthorized: true } });
+authRouter.post("/silent-login", verifyAuth(), (req, res) => {
+  const id = (req.user as ExpressUser).id;
+  res.json({ data: { id } });
 });
 
 export { authRouter };
