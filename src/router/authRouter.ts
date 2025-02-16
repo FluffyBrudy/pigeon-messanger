8ac29a5ec16dc19edc8f3f3bf898969a8681d699
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-02-16 15:39:10 +0545

    Remove hardcoded route path

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { RegisterController } from "../controller/auth/RegisterController";
 import { loginValidator, registerValidator } from "../validator/auth/auth";
 import { LoginController } from "../controller/auth/LoginController";
+import { AUTH } from "./constants";
 
 const authRouter = Router();
 
-authRouter.post("/register", registerValidator, RegisterController);
-authRouter.post("/login", loginValidator, LoginController);
+authRouter.post(AUTH.REGISTER, registerValidator, RegisterController);
+authRouter.post(AUTH.LOGIN, loginValidator, LoginController);
 
 export { authRouter };
