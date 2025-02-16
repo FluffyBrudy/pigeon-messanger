779a2959344394476d228cfa831c0c13373d1c0a
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-02-16 15:38:51 +0545

    Add chat route

--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -3,7 +3,8 @@ import { authRouter } from "./authRouter";
 import { socialRouter } from "./socialRouter";
 import { silentRouter } from "./silentRouter";
 import { verifyAuth } from "../middleware/authVerification";
-import { AUTH, SILENT, SOCIAL } from "./constants";
+import { AUTH, CHAT, SILENT, SOCIAL } from "./constants";
+import { chatRouter } from "./chatRouter";
 
 const apiRouter = Router();
 
@@ -12,5 +13,6 @@ apiRouter.use(AUTH.ROOT, authRouter);
 apiRouter.use(verifyAuth());
 apiRouter.use(SILENT.ROOT, silentRouter);
 apiRouter.use(SOCIAL.ROOT, socialRouter);
+apiRouter.use(CHAT.ROOT, chatRouter);
 
 export { apiRouter };
