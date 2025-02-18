f4839d8b178b6c6594365fc668a641f09ebc032c
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-02-18 18:00:18 +0545

    remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen(3000, () => {
-    console.log("Listening at: http://localhost:3000");
-});
-exports.default = app_1.app;
--- dist/validator/chat/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RECIPIENT_ID = exports.MESSAGE = void 0;
-exports.MESSAGE = "message";
-exports.RECIPIENT_ID = "recipientId";
