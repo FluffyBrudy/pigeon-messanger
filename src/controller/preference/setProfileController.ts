6dc6a5f8bd3cd860c2281584cefffba525b45c86
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-03-02 22:08:38 +0545

    Rename setProfileController.ts and add GetProfileSignatureController
    - Renamed the file from src/controller/profileAndPreferance/setProfileController.ts to src/controller/preference/setProfileController.ts.
    - Added a new controller function GetProfileSignatureController to generate and return a profile signature.

--- src/controller/preference/setProfileController.ts
+++ src/controller/preference/setProfileController.ts
@@ -1,6 +1,16 @@
 import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
 import { BodyValidationError } from "../../error/error";
+import { createProfileSignature } from "../utils/signature";
+
+export const GetProfileSignatureController: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const signature = createProfileSignature();
+  res.json({ data: signature });
+};
 
 export const setInitProfileController: RequestHandler = async (
   req,
