84a3064088358d9dc93807ab40e801a6360bb901
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-03-13 19:33:11 +0545

    add user data in silent router response

--- dist/router/silentRouter.js
+++ dist/router/silentRouter.js
@@ -1,11 +1,45 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.silentRouter = void 0;
 const express_1 = require("express");
 const constants_1 = require("./constants");
+const error_1 = require("../error/error");
+const dbClient_1 = require("../service/dbClient");
+const constants_2 = require("../controller/auth/constants");
 const silentRouter = (0, express_1.Router)();
 exports.silentRouter = silentRouter;
-silentRouter.post(constants_1.SILENT.LOGIN, (req, res) => {
+silentRouter.post(constants_1.SILENT.LOGIN, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
     const id = req.user.id;
-    res.json({ data: { id } });
-});
+    try {
+        const user = yield dbClient_1.dbClient.user.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                password: true,
+                username: true,
+                profile: { select: { initialized: true, picture: true } },
+            },
+        });
+        if (!user)
+            return next(new error_1.ApiError(401, constants_2.INVALID_CREDENTIALS, true));
+        res.json({
+            id: user.id,
+            username: user.username,
+            initialized: (_a = user.profile) === null || _a === void 0 ? void 0 : _a.initialized,
+            imageUrl: (_b = user.profile) === null || _b === void 0 ? void 0 : _b.picture,
+        });
+    }
+    catch (err) {
+        return next(new error_1.LoggerApiError(err, 500));
+    }
+}));
--- src/router/silentRouter.ts
+++ src/router/silentRouter.ts
@@ -1,11 +1,34 @@
 import { Router } from "express";
 import { ExpressUser } from "../types/common";
 import { SILENT } from "./constants";
+import { ApiError, LoggerApiError } from "../error/error";
+import { dbClient } from "../service/dbClient";
+import { INVALID_CREDENTIALS } from "../controller/auth/constants";
 
 const silentRouter = Router();
-silentRouter.post(SILENT.LOGIN, (req, res) => {
+silentRouter.post(SILENT.LOGIN, async (req, res, next) => {
   const id = (req.user as ExpressUser).id;
-  res.json({ data: { id } });
+  try {
+    const user = await dbClient.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        password: true,
+        username: true,
+        profile: { select: { initialized: true, picture: true } },
+      },
+    });
+    if (!user) return next(new ApiError(401, INVALID_CREDENTIALS, true));
+
+    res.json({
+      id: user.id,
+      username: user.username,
+      initialized: user.profile?.initialized,
+      imageUrl: user.profile?.picture,
+    });
+  } catch (err) {
+    return next(new LoggerApiError(err, 500));
+  }
 });
 
 export { silentRouter };
